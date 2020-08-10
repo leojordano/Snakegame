600500b6bc1868f8636dc830e49e4e0061bcf581
leojordano <leonardojordano1@gmail.com>
2020-08-10 14:02:49 -0300

    Jogo finalizado

---
+++ script.js
@@ -0,0 +1,113 @@
+let canvas = document.getElementById('snake')
+let context = canvas.getContext('2d')
+let box = 32
+let snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+let bestPoints = document.getElementById('best')
+let bestValue = localStorage.getItem('snakeGame_points')
+
+let pointsValue = 0
+
+let direction = 'right'
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y:  Math.floor(Math.random() * 15 + 1) * box
+}
+
+function criarBG() {
+    context.fillStyle = 'lightgreen'
+    context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+function criarCobra() {
+    for(i = 0; i < snake.length; i++) {
+        context.fillStyle = 'green'
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
+var color = 'red'
+function getColorFood() {
+    let colors = ['red', 'blue', 'purple', 'orange', 'yellow']
+    color = colors[Math.floor(Math.random() * colors.length)]
+}
+
+function drawFood() {
+    context.fillStyle = color
+    context.fillRect(food.x, food.y, box, box)
+}
+
+function setPoints() {
+    let points = document.getElementById('points')
+    points.innerText = pointsValue
+}
+
+document.addEventListener('keydown', update)
+
+function update(e) {
+    if(e.keyCode == 37 && direction != 'right') direction = 'left' 
+    if(e.keyCode == 38 && direction != 'down') direction = 'up' 
+    if(e.keyCode == 39 && direction != 'left') direction = 'right' 
+    if(e.keyCode == 40 && direction != 'up') direction = 'down' 
+}
+
+function iniciar() {
+    if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box
+
+    if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
+
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo)
+
+            if(pointsValue > bestValue) {
+                localStorage.setItem('snakeGame_points', pointsValue)
+            }
+
+            alert('Game Over =(')
+            location.reload()
+        }
+    }
+
+    if(bestPoints) {
+        bestPoints.innerText = bestValue
+    }
+
+    criarBG()
+    criarCobra()
+    setPoints()
+    drawFood()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction == 'right') snakeX += box
+    if(direction == 'left') snakeX -= box
+    if(direction == 'up') snakeY -= box
+    if(direction == 'down') snakeY += box
+
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop()
+    } else {
+        pointsValue++
+        setPoints()
+        getColorFood()
+        food.x = Math.floor(Math.random() * 15 + 1) * box
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciar, 100)
